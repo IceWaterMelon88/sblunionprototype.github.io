e94f6c79ce40e7e17cf1882d0dfa490c6bb9d61f
DESKTOP-OE0F5HS\PCS <icewatermelon88@gmail.com>
2025-10-27 11:29:07 +0900

    sblunionprototype

---
+++ _next/static/75Pf-11H66tZOhBMIrIMO/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/6d99a2a4a6b50a14.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sblunionprototype.github.io//_next/:path+",
+        "destination": "/sblunionprototype.github.io/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/6d99a2a4a6b50a14.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/94a8ccf9f327ed6a.js",
+  "static/chunks/turbopack-ddc85fb3563a60dd.js"
+])
