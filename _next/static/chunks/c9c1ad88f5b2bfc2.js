640fe28af822e9d6df3f7058aded3e27ced0e84e
DESKTOP-OE0F5HS\PCS <icewatermelon88@gmail.com>
2025-12-09 13:53:36 +0900

    Final fix for file mode issue after re-clone

---
+++ _next/static/chunks/c9c1ad88f5b2bfc2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/dcd3a07d9d322c9a.js",
+  "static/chunks/turbopack-393715e8a7d0a9d8.js"
+])
--- _next/static/chunks/f04fd21bd530935d.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/b89cd47ebab4302c.js",
-  "static/chunks/turbopack-a03c550a8d1035da.js"
-])
